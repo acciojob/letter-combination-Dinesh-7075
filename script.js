01bb775d01f2f0209cdf879b8e268f25e4c0a4de
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-09-28 13:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	map={
+    "2":"abc",
+    "3":"def",
+    "4":"ghi",
+    "5":"jkl",
+    "6":"mno",
+    '7':"pqrs",
+    "8":"tuv",
+    "9":"wxyz"
+}
+
+
+	solution =[];
+    if(input.length==0) return solution;
+    function backTrack(indx ,a){
+        if(indx>=input.length){
+            solution.push(a);
+            return;
+        }
+        for(let i=0;i<map[input[indx]].length;i++){
+            backTrack(indx+1,a+map[input[indx]][i]);
+        }
+    }
+    backTrack(0,"");
+    return solution;
 }
 
 module.exports = letterCombinations;
